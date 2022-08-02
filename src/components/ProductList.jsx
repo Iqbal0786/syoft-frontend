e5a4fda8160dfefdb7091dbd912ddc3f63ebc10a
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-02 17:36:55 +0530

    some changes

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,16 +1,32 @@
 import { AppBar, Toolbar } from '@mui/material';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import ErroImage from './ErroImage';
 
 export default function ProductList() {
+   const [isAllowd,setIsAllowd]=useState(true)
+   const navigate= useNavigate()
   useEffect(()=>{
      let userdata= JSON.parse(sessionStorage.getItem("user"));
-     console.log(userdata)
+      if(userdata){
+        let isStaff= userdata.user.roles?.includes("staff");
+        if(isStaff){
+         setIsAllowd(false)
+        }
+      }
+      else{
+       navigate("/login")
+      }
   },[])
   return (
    <>
+   { 
+   isAllowd==false?<ErroImage/>:<>
     <AppBar>
       <Toolbar>Welcome to Syoft.shop</Toolbar>
     </AppBar>
+   </>
+   }
    </>
   )
 }
